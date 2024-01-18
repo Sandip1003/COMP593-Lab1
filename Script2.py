8e452657e1aac1bee764f6b1b6707768a6b6ff34
Sandip1003 <156461869+Sandip1003@users.noreply.github.com>
2024-01-17 22:54:43 -0500

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added one print() statement to script2.py

--- Script2.py
+++ Script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Special cases aren't special enough to break the rules.")
 print(" Errors should never pass silently.")
+print("Namespaces are one honking great idea -- let's do more of those!")
